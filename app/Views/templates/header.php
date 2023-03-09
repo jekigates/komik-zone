7d9422e6042a2171a9353b1cffc5de0e18491362
Jeki Gates <jekigates2004@gmail.com>
2023-03-09 20:49:34 +0700

    change fonts to poppins

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,6 +4,29 @@
     <title>CodeIgniter Tutorial</title>
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/bootstrap.min.css') ?>">
     <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+    <style>
+        @font-face {
+            font-family: 'Poppins';
+            src: url('../fonts/Poppins-Regular.ttf') format('truetype');
+            font-weight: 400;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'Poppins';
+            src: url('../fonts/Poppins-Bold.ttf') format('truetype');
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        * {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1><?= esc($title) ?></h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <button class="btn btn-primary fw-bold">KomikZone</button>
+        </div>
+    </nav>
