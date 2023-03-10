082006c36a81e1ec7c22b3107ea2546fc6092959
Jeki Gates <jekigates2004@gmail.com>
2023-03-10 08:51:27 +0700

    change the carousel automatic slides

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,5 +1,5 @@
 <div class="container-fluid container-lg">
-    <div id="carouselExampleCaptions" class="carousel slide mt-4">
+    <div id="carouselExampleCaptions" class="carousel slide carousel-fade mt-4" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button
                 type="button"
@@ -13,21 +13,21 @@
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-            <div class="carousel-item active">
+            <div class="carousel-item active" data-bs-interval="5000">
                 <img src="<?= base_url('images/banners/one-piece.jpg') ?>" class="d-block w-100 img-fluid carousel-img" alt="" />
                 <div class="carousel-caption">
                     <h2 class="fw-bold">One Piece</h2>
                     <p>Bergabunglah dengan Luffy dan kru di petualangan epik mereka!</p>
                 </div>
             </div>
-            <div class="carousel-item">
+            <div class="carousel-item" data-bs-interval="5000">
                 <img src="<?= base_url('images/banners/my-hero-academia.jpg') ?>" class="d-block w-100 img-fluid carousel-img" alt="" />
                 <div class="carousel-caption">
                     <h2 class="fw-bold">My Hero Academia</h2>
                     <p>Teman-teman SMA berjuang memperoleh kekuatan super untuk menjadi pahlawan.</p>
                 </div>
             </div>
-            <div class="carousel-item">
+            <div class="carousel-item" data-bs-interval="5000">
                 <img src="<?= base_url('images/banners/spy-x-family.jpg') ?>" class="d-block w-100 img-fluid carousel-img" alt="" />
                 <div class="carousel-caption">
                     <h2 class="fw-bold">Spy X Family</h2>
