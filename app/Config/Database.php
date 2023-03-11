ee6a192c4d62dd1d43df198d42e916091b93467b
Jeki Gates <jekigates2004@gmail.com>
2023-03-11 23:38:44 +0700

    connect database and create migrations

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
-        'database' => '',
+        'database' => 'komik_zone',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
