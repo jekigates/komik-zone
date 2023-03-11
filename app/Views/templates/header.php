c87344c08c1b830255cbdbe4ead12eb5db26199e
Jeki Gates <jekigates2004@gmail.com>
2023-03-11 08:58:42 +0700

    change nav-link active based on page name

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -121,13 +121,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto my-2 my-lg-0">
                         <li class="nav-item me-lg-4">
-                            <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">Home</a>
+                            <a class="nav-link <?= ($title == 'Home') ? 'active' : '' ?>" aria-current="page" href="<?= base_url() ?>">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?= base_url('daftar_komik') ?>">Daftar Komik</a>
+                            <a class="nav-link <?= ($title == 'Daftar Komik') ? 'active' : '' ?>" href="<?= base_url('daftar_komik') ?>">Daftar Komik</a>
                         </li>
                         <li class="nav-item ms-lg-4">
-                            <a class="nav-link" href="<?= base_url('faq') ?>">FAQ</a>
+                            <a class="nav-link <?= ($title == 'Faq') ? 'active' : '' ?>" href="<?= base_url('faq') ?>">FAQ</a>
                         </li>
                     </ul>
                     <form class="d-flex" role="search" action="<?= base_url('daftar_komik') ?>">
